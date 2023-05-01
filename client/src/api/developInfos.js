71af95a2c3e2a4edb75b519890e167052c40a04f
rlwjd31 <rlwjd31@naver.com>
2023-05-02 02:54:38 +0900

    🚀[Chore]: react-spinners 추가

---
+++ client/src/api/developInfos.js
@@ -0,0 +1,9 @@
+// TODO: develop VS Prod mode에 따른 프로젝트 환경 셋팅
+// TODO: 임시적으로 json-server를 이용하여 비동기적으로 data 가져오기!!
+const BASE_URL = 'http://localhost:4000';
+
+const getDevelopInfo = () => {
+  return console.log('later!!');
+};
+
+export default {};
