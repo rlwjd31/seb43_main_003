c7b035321ceaa89026555fdce87522c029cfe3a7
ksr0818 <ksr940818@gmail.com>
2023-05-03 17:27:52 +0900

    🚀[Feat] Member profileImage 초기 값 설정

--- server/src/main/java/server/mainproject/member/entity/Member.java
+++ server/src/main/java/server/mainproject/member/entity/Member.java
@@ -27,9 +27,11 @@ public class Member extends Auditable {
     @Column
     private String nickName;
 
-    @Column
-    private String profileImage;
+    @Column(nullable = false)
+    private String ProfileImage = "https://mblogthumb-phinf.pstatic.net/MjAyMDA2MTBfMTY1/MDAxNTkxNzQ2ODcyOTI2.Yw5WjjU3IuItPtqbegrIBJr3TSDMd_OPhQ2Nw-0-0ksg.8WgVjtB0fy0RCv0XhhUOOWt90Kz_394Zzb6xPjG6I8gg.PNG.lamute/user.png?type=w800";
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    private List<String> roles = new ArrayList<>();
+
+
+//    @ElementCollection(fetch = FetchType.EAGER)
+//    private List<String> roles = new ArrayList<>();
 }
