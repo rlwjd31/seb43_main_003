efc70c53f43d41d1028871cf99e34ee1f40ab336
rlwjd31 <rlwjd31@naver.com>
2023-05-09 03:04:26 +0900

    🚀[Chore]: lint -> prop-types & no-spreading 해제

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -17,5 +17,7 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': [1, { custom: 'ignore' }],
   },
 };
