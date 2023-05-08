fc22f205fb0519907982b225d00d793010b96630
rlwjd31 <rlwjd31@naver.com>
2023-05-09 01:57:32 +0900

    💄[Style]: gray color 추출

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black1: '#OAOAOA',
+        black2: '#111111',
+        black3: '#222222',
+        gray1: '#ECE9E7',
+        gray2: '#DCDAD6',
+        gray3: '#B1B1B1',
+        gray4: '#555555',
+        gray5: '#595959',
+        gray6: '#777777',
+        gray7: '#787878',
+        gray8: '#7A7A7A',
+        activeBlue: '#0045F6',
+      },
+    },
   },
   plugins: [],
 };
