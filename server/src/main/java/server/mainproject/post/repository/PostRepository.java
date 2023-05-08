0b582c90fd3a41e7a80217fe49a6e1ca23fe5411
ksr0818 <ksr940818@gmail.com>
2023-05-08 18:15:34 +0900

    🚀[Remove] postMapper 및 postRepository 삭제 (DevPost로 변경된 상황)

--- server/src/main/java/server/mainproject/post/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package server.mainproject.post.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import server.mainproject.post.entity.DevPost;
-
-public interface PostRepository extends JpaRepository<DevPost, Long> {
-}
