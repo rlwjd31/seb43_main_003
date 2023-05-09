8b0bfe4ef72043ab3e3ec4357d6a71f4d31adcab
ksr0818 <ksr940818@gmail.com>
2023-05-09 13:23:15 +0900

    🚀[Remove] Client 디렉토리 삭제

--- client/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import { RouterProvider } from 'react-router-dom';
-
-import routers from './router';
-
-export default function App() {
-  return <RouterProvider router={routers} />;
-}
--- client/src/pages/PageNotFound.jsx
+++
@@ -1,5 +0,0 @@
-function PageNotFound() {
-  return <h1 className="text-3xl font-bold underline">PageNotFound Page</h1>;
-}
-
-export default PageNotFound;
