2f56f0cfec9995333dadff8be3a42c30e026d6bf
rlwjd31 <rlwjd31@naver.com>
2023-05-11 03:03:50 +0900

    🚀[Chore]: flowbite 설치

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -29,5 +30,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line global-require
+  plugins: [require('flowbite/plugin')],
 };
