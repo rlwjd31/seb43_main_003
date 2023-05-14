1b53edd8737df59965846559d28b7a4b415b7edd
rlwjd31 <rlwjd31@naver.com>
2023-05-14 21:16:14 +0900

    💄[Style]: main banner 수정

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,14 +26,13 @@ function Home() {
         <div className="w-full flex justify-center bg-white1">
           <div className="flex w-full justify-between max-w-limit pt-24 pb-32 animate-fadeIn-up">
             <h2 className="text-4xl font-bold">개발자를 위한 공간.</h2>
-            <div className="flex flex-col text-sm font-normal max-w-[26rem]">
+            <div className="flex flex-col text-sm font-normal max-w-[26rem] tracking-[0.04rem] space-y-[0.5rem]">
               <p>
-                이곳은 개발자들을 위한 <span className="text-activeBlue">공간</span>
+                이곳은 개발자들을 위한 <span className="inline font-extrabold">공간</span>
                 입니다.
               </p>
-              <p>자유롭게 탐색하고, 유용한 정보들을 찾아가세요.</p>
-              <p>당신의 지식을 나누어주세요.</p>
-              <p>개발에 대한 정보라면 어떤 것이든 환영합니다.</p>
+              <p>자유롭게 탐색하고, 유용한 정보들을 찾아가세요. 당신의 지식을</p>
+              <p>나누어주세요. 개발에 대한 정보라면 어떤 것이든 환영합니다.</p>
             </div>
           </div>
         </div>
