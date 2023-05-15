e2c35d46301a641ca47af100ce572df330e26b6f
ksr0818 <ksr940818@gmail.com>
2023-05-15 10:27:27 +0900

    🚀[Feat] RecommendResponseDto 생성

---
+++ server/src/main/java/server/mainproject/post/dto/RecommendResponseDto.java
@@ -0,0 +1,28 @@
+package server.mainproject.post.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import server.mainproject.comment.dto.CommentDto;
+import server.mainproject.member.dto.AuthorResponseDto;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class RecommendResponseDto {
+    private Long recommendsId;
+    private Long postId;
+    private Long memberId;
+    private String title;
+    private String link;
+    private int star;
+    private Double starAvg;
+    private int recommend;
+    private List<AuthorResponseDto> authors;
+    private List<Post_TagResponseDto> postTags;
+    private List<CommentDto.ResponseComment> comments;
+
+}
+
