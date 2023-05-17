4ac6c736efc63fdd2d4a324b412e09d2c40618e0
UHA <110329914+yuhallo@users.noreply.github.com>
2023-05-17 23:10:11 +0900

    💄[Style] : page not found img 추가

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,5 +1,14 @@
+import SadCatImg from '../assets/notFoundCat.png';
+
 function PageNotFound() {
-  return <h1 className="text-3xl font-bold underline">PageNotFound Page</h1>;
+  return (
+    <div className="flex flex-col items-center my-[10rem]">
+      <h1 className="text-[5rem] text-gray6 font-bold underline mb-[1rem]">
+        Page Not Found
+      </h1>
+      <img src={SadCatImg} alt="" />
+    </div>
+  );
 }
 
 export default PageNotFound;
