573d8a0671d75f1718605c8c97cf07ca51633ee7
rlwjd31 <rlwjd31@naver.com>
2023-05-17 19:19:23 +0900

    🚀[Feat]: Main Page State redux toolkit 비동기 연결 구현

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import popularSlice from './popular';
 import realtimeRankingSlice from './realtimeRanking';
+import developmentsSlice from './developmentSlice';
 
 const store = configureStore({
   reducer: {
     popular: popularSlice.reducer,
     ranking: realtimeRankingSlice.reducer,
+    development: developmentsSlice.reducer,
   },
 });
 
