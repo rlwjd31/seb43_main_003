391aec931c7ac6b080188a7fd0e10a4de6bb378e
rlwjd31 <rlwjd31@naver.com>
2023-05-22 04:51:38 +0900

    🔨[Refactor]: 전체개발페이지 refactoring

---
+++ client/src/components/UI/Accordian.jsx
@@ -0,0 +1,32 @@
+import Button from './Button';
+
+function Accordian({
+  visible,
+  onClickHandler,
+  activeValue,
+  itemList,
+  className,
+  activeColor,
+}) {
+  return (
+    <>
+      {visible &&
+        itemList.map((value, index) => (
+          <Button
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
+            onClick={() => onClickHandler(value)}
+            className={`${className} ${activeValue === value ? activeColor : 'inherit'}`}
+          >
+            {value}
+          </Button>
+        ))}
+      {}
+    </>
+  );
+}
+
+export default Accordian;
+
+// eslintreact/jsx-no-useless-fragment나는 경우 -> https://codesandbox.io/s/qw6cj
+// {}를 이용하여 lint에러를 해결함
--- client/src/pages/AllDevelopments.jsx
+++ client/src/pages/AllDevelopments.jsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { Link } from 'react-router-dom';
 import Aside from '../components/layout/Aside';
 import Card from '../components/UI/Card';
 import Item from '../components/Item';
 import { fetchAllDevelopmentsAction } from '../store/developmentSlice';
 import { ChevronDownIcon, PencilIcon } from '../components/Icons';
+import Button from '../components/UI/Button';
+import Accordian from '../components/UI/Accordian';
 
 function AllDevelopments() {
   const [sortaActive, setSortaActive] = useState('전체');
+  const [accordianVisible, setAccordianVisible] = useState(false);
+  const [accordianFilterValue, setAccordianFilterValue] = useState('최신순');
   const { allDevelopments } = useSelector(state => state.developments);
   const dispatch = useDispatch();
 
   const onSortaButtonClickHandler = e => setSortaActive(prev => e.target.value);
-  // const sortaButtonStyle = sortaActive;
-  // const sortaButtonStyle = buttonValue =>
-  //   `py-[0.375rem] px-4 border-[1px] border-solid border-gray8  rounded-2xl ${
-  //     sortaActive === buttonValue ? 'bg-black3 text-white1' : 'bg-white1 text-black3'
-  //   }`;
+
+  const onAccordianFilterVisibleClickHandler = () => setAccordianVisible(prev => !prev);
+  const onAccordianFilterValueClickHandler = filterValue =>
+    setAccordianFilterValue(prev => filterValue);
+  // const onAcordian
   const activeStyle = 'bg-black3 text-white1';
   const notActiveStyle = 'bg-white1 text-black3';
 
@@ -27,87 +32,76 @@ function AllDevelopments() {
 
   return (
     <>
-      <Aside
-        title="Tech Stack"
-        categories={{
-          titles: [
-            'javascript',
-            'typescript',
-            'Authentication',
-            'Webpack',
-            'Css-in-js',
-            'React',
-            'Vue',
-            'Angular',
-            'Nextjs',
-            'Database',
-            'Network',
-            'Algorithm',
-          ],
-        }}
-      />
-      <div className="pt-main-top w-full">
-        <div className="flex justify-between mt-3">
+      <section>
+        <Aside
+          title="Tech Stack"
+          categories={{
+            titles: [
+              'javascript',
+              'typescript',
+              'Authentication',
+              'Webpack',
+              'Css-in-js',
+              'React',
+              'Vue',
+              'Angular',
+              'Nextjs',
+              'Database',
+              'Network',
+              'Algorithm',
+            ],
+          }}
+        />
+      </section>
+      <section className="pt-main-top w-full">
+        <div className="relative flex justify-between mt-3">
           <div className="flex gap-2">
-            <button
-              onClick={onSortaButtonClickHandler}
-              type="button"
-              className={`text-xs px-3 border-[1px] border-solid border-gray8 rounded-2xl ${
-                sortaActive === '전체' ? activeStyle : notActiveStyle
-              }`}
-              value="전체"
-            >
-              전체
-            </button>
-            <button
-              onClick={onSortaButtonClickHandler}
-              type="button"
-              className={`text-xs px-3 border-[1px] border-solid border-gray8 rounded-2xl ${
-                sortaActive === '글' ? activeStyle : notActiveStyle
-              }`}
-              value="글"
-            >
-              글
-            </button>
-            <button
-              onClick={onSortaButtonClickHandler}
-              type="button"
-              className={`text-xs px-3 border-[1px] border-solid border-gray8 rounded-2xl ${
-                sortaActive === '영상' ? activeStyle : notActiveStyle
-              }`}
-              value="영상"
-            >
-              영상
-            </button>
-            <button
-              onClick={onSortaButtonClickHandler}
-              type="button"
-              className={`text-xs px-3 border-[1px] border-solid border-gray8 rounded-2xl ${
-                sortaActive === '트렌드' ? activeStyle : notActiveStyle
-              }`}
-              value="트렌드"
-            >
-              트렌드
-            </button>
+            {['전체', '글', '영상', '트렌드'].map((filterValue, index) => (
+              <Button
+                // eslint-disable-next-line react/no-array-index-key
+                key={index}
+                value={filterValue}
+                onClick={e => onSortaButtonClickHandler(e)}
+                className={`text-xs px-3 py-1 border-[1px] border-solid border-gray8 rounded-2xl ${
+                  sortaActive === filterValue ? activeStyle : notActiveStyle
+                }`}
+              >
+                {filterValue}
+              </Button>
+            ))}
           </div>
-          <div className="flex items-center border-[1px] border-gray6 border-solid p-2 text-[0.625rem] cursor-pointer">
-            <span className="mr-2">최신순</span> <ChevronDownIcon />
+          <div className="absolute top-0 right-0 flex flex-col border-[1px] border-gray6 border-solid text-[0.625rem] cursor-pointer">
+            <Button onClick={onAccordianFilterVisibleClickHandler}>
+              <span className="p-2">최신순</span> <ChevronDownIcon className="mr-2" />
+            </Button>
+            {accordianVisible && (
+              <Accordian
+                onClickHandler={onAccordianFilterValueClickHandler}
+                activeValue={accordianFilterValue}
+                itemList={['최신순', '인기순']}
+                className="border-t-[1px] p-2 border-gray6 border-solid"
+                activeColor="text-activeBlue"
+                visible={accordianVisible}
+              />
+            )}
           </div>
         </div>
         <div className="w-full flex flex-col max-w-limit">
-          <h3 className="flex items-center text-[1.2rem] py-7 mt-5 border-b-[1px] border-solid border-gray4">
-            <PencilIcon className="w-5 h-5 mr-3" />{' '}
-            <span className="font-bold">글 쓰기</span>
+          <h3 className="flex items-center text-[1.2rem] mt-5 border-b-[1px] border-solid border-gray4">
+            <Link to="/developments/new" className="flex w-32 py-7 pr-4">
+              <PencilIcon className="w-5 h-5 mr-3" />{' '}
+              <span className="font-bold">글 쓰기</span>
+            </Link>
           </h3>
-          <div className="w-full flex justify-between flex-wrap gap-4 py-14">
+          <div className="w-full flex justify-between flex-wrap gap-6 py-8">
             {allDevelopments.data.map(info => (
-              <Card key={info.postId} width="30%">
+              <Card key={info.postId} flexItemwidth="30%">
                 <Item {...info} />
               </Card>
             ))}
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
